9e8bf6769cbbff69bcceff6f894d6c31634a7881
longj18 <maxsoccerssport@hotmail.com>
2022-04-21 14:01:10 -0400

    push

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -49,39 +49,70 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
+/////////  MAP - is a data structure that we can use to map data to keys /////
+
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open')
+  .set(false, 'We are close :(');
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+// rest.clear();
+
+const arr = [1, 2];
+rest.set(arr, 'Test');
+rest.set(document.querySelector('h1', 'heading'));
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(arr));
 
 ///////// Sets -  collection of unique values ////////////////////////
 
-const ordersSet = new Set([
-  'Pasta',
-  'Pizza',
-  'Pizza',
-  'Risotto',
-  'Pasta',
-  'Pizza',
-]);
+// const ordersSet = new Set([
+//   'Pasta',
+//   'Pizza',
+//   'Pizza',
+//   'Risotto',
+//   'Pasta',
+//   'Pizza',
+// ]);
 
-console.log(ordersSet);
-console.log(new Set());
-console.log(new Set('Max'));
-console.log(ordersSet.size);
-console.log(ordersSet.has('Pizza'));
-console.log(ordersSet.has('Bread'));
-ordersSet.add('Garlic Bread');
-ordersSet.add('Garlic Bread');
-ordersSet.delete('Risotto');
-// ordersSet.clear();
-console.log(ordersSet);
+// console.log(ordersSet);
+// console.log(new Set());
+// console.log(new Set('Max'));
+// console.log(ordersSet.size);
+// console.log(ordersSet.has('Pizza'));
+// console.log(ordersSet.has('Bread'));
+// ordersSet.add('Garlic Bread');
+// ordersSet.add('Garlic Bread');
+// ordersSet.delete('Risotto');
+// // ordersSet.clear();
+// console.log(ordersSet);
 
-for (const order of ordersSet) console.log(order);
+// for (const order of ordersSet) console.log(order);
 
-// Example
-const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
-// const staffSet = [...new Set(staff)];
-// console.log(staffSet);
-console.log(new Set(staff).size);
+// // Example
+// const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+// // const staffSet = [...new Set(staff)];
+// // console.log(staffSet);
+// console.log(new Set(staff).size);
 
-console.log(new Set('josephmaxwelllong').size);
+// console.log(new Set('josephmaxwelllong').size);
 
 //====================================================================
 //Challenge 2.
