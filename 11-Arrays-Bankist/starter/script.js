b1f4d4baed5de90ec5450df1e08e1a107ac0d40a
longj18 <maxsoccerssport@hotmail.com>
2022-04-27 13:37:21 -0400

    push

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -72,20 +72,56 @@ const displayMovements = function (movements) {
       <div class="movements__type movements__type--${type}">${
       index + 1
     } ${type}</div>
-      <div class="movements__value">${movement}</div>
+      <div class="movements__value">${movement}€</div>
     </div>
         `;
     containerMovements.insertAdjacentHTML('afterbegin', html);
   });
 };
-
 displayMovements(account1.movements);
+
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, cur) => acc + cur, 0);
+  labelBalance.textContent = `${balance} €`;
+};
+calcDisplayBalance(account1.movements);
+
+const calcDisplaySummary = function (movements) {
+  const incomes = movements
+    .filter(movement => movement > 0)
+    .reduce((acc, movement) => acc + movement, 0);
+  labelSumIn.textContent = `${incomes}€`;
+
+  const out = movements
+    .filter(movement => movement < 0)
+    .reduce((acc, movement) => acc - movement, 0);
+  labelSumOut.textContent = `${out}€`;
+
+  const interest = movements
+    .filter(movement => movement > 0)
+    .map(deposit => (deposit * 1.2) / 100)
+    .filter(interest => interest > 1)
+    .reduce((acc, interest) => acc + interest, 0);
+  labelSumInterest.textContent = `${interest}`;
+};
+calcDisplaySummary(account1.movements);
+
+const createUsernames = function (accounts) {
+  accounts.forEach(function (account) {
+    account.username = account.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+createUsernames(accounts);
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 
 // Challenge 1
-
+/*
 const checkDogs = function (dogsJulia, dogsKate) {
   const correctDogs = dogsJulia.slice(1, 3);
   const dogs = [...correctDogs, ...dogsKate];
@@ -100,7 +136,7 @@ const checkDogs = function (dogsJulia, dogsKate) {
   });
 };
 checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
-
+*/
 // LECTURES
 
 // const currencies = new Map([
@@ -218,3 +254,136 @@ currenciesUnique.forEach(function (value, _, map) {
 });
 
 */
+
+// Data Transformation: MAP, FILTER, REDUCE
+
+// MAP - returns a new array containing the results of applying an operation on all original array elements.
+
+// FILTER - returns a new array containing the array elements that passed a specofoed test condition.
+
+// REDUCE - boils ('reduces') all array elements down to one single value(e.g. adding all elements together)
+
+///////////////////// The MAP METHOD /////////////////////////////
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const eurToUsd = 1.1;
+
+// const movementsUsd = movements.map(function (movements) {
+//   return movements * eurToUsd;
+// });
+
+/*
+//arrow function with map method
+const movementsUsd = movements.map(movements =>
+  Math.trunc(movements * eurToUsd)
+);
+
+console.log(movements);
+console.log(movementsUsd);
+
+const movementsUsdfor = [];
+
+for (const mov of movements) movementsUsdfor.push(mov * eurToUsd);
+
+console.log(movementsUsdfor);
+
+const movementsDescriptions = movements.map(
+  (mov, i) =>
+    `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
+      mov
+    )}`
+);
+
+console.log(movementsDescriptions);
+*/
+
+//////////////////// FILTER METHOD ////////////////////////
+/*
+console.log(movements);
+
+const deposits = movements.filter(movement => movement > 0);
+console.log(deposits);
+
+const withdrawals = movements.filter(movement => movement < 0);
+console.log(withdrawals);
+*/
+// const depositsFor = [];
+// for (const movement of movements) if (movement > 0) depositsFor.push(movement);
+// console.log(depositsFor);
+
+//////////////////// REDUCE METHOD /////////////////////////
+/*
+//accumulator is like a snowball that accumulates thee sum of the array
+const balance = movements.reduce((acc, cur) => acc + cur, 0);
+console.log(balance);
+
+let balance2 = 0;
+for (const mov of movements) balance2 += mov;
+console.log(balance2);
+
+//Maximum value
+
+const maximum = movements.reduce((acc, mov) => (acc > mov ? acc : mov));
+console.log(maximum);
+*/
+
+/*
+//Coding Challenge 3
+const ages1 = [5, 2, 4, 1, 15, 8, 3]; // Julias
+const ages2 = [16, 6, 10, 5, 6, 1, 4]; // Kates
+const calcAverageHumanAge = ages =>
+  ages
+    .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter(age => age >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+console.log(calcAverageHumanAge(ages1));
+console.log(calcAverageHumanAge(ages2));
+*/
+
+// Coding Challenge 2
+/*
+const data1 = [5, 2, 4, 1, 15, 8, 3]; // Julias
+const data2 = [16, 6, 10, 5, 6, 1, 4]; // Kates
+const calcAverageHumanAge = function (ages) {
+  const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
+  const adults = humanAges.filter(age => age >= 18);
+  const avgAge = Math.trunc(
+    adults.reduce((acc, age) => acc + age, 0) / adults.length
+  );
+  return avgAge;
+};
+console.log(calcAverageHumanAge(data1));
+console.log(calcAverageHumanAge(data2));
+*/
+// Challenge 1
+/*
+const checkDogs = function (dogsJulia, dogsKate) {
+  const correctDogs = dogsJulia.slice(1, 3);
+  const dogs = [...correctDogs, ...dogsKate];
+  dogs.forEach(function (dog, index) {
+    if (dog >= 3) {
+      console.log(
+        `Dog number ${index + 1} is an adult, and is ${dog} years old`
+      );
+    } else {
+      console.log(`Dog number ${index + 1} is still a puppy 🐶`);
+    }
+  });
+};
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+*/
+
+///////////// The magic of Chaining Methods //////////////
+// const eutoToUsd = 1.1;
+
+// //PIPELINE
+// const totalDepositsUSD = movements
+//   .filter(movement => movement > 0)
+//   .map((movement, i, arr) => {
+//     // console.log(arr);
+//     return movement * eurToUsd;
+//   })
+//   // .map(movement => movement * eurToUsd)
+//   .reduce((acc, movement) => acc + movement, 0);
+
+// console.log(totalDepositsUSD);
