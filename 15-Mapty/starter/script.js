f2182e27dc0518cce861561e2d74c5b6466f5a08
longj18 <maxsoccerssport@hotmail.com>
2022-05-03 21:43:12 -0400

    push

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -11,45 +11,77 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let mapEvent, map;
+
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
     function (position) {
       const { latitude } = position.coords;
       const { longitude } = position.coords;
-      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+      //   console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
       const coords = [latitude, longitude];
-      const map = L.map('map').setView(coords, 13);
-      //   console.log(map);
+      let map = L.map('map').setView(coords, 13);
       L.tileLayer('https://{s}.tile.openstreetmap.fr/hot//{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
-
-      map.on('click', function (mapEvent) {
-        const { lat, lng } = mapEvent.latlng;
-
-        L.marker([lat, lng])
-          .addTo(map)
-          .bindPopup(
-            L.popup({
-              maxWidth: 250,
-              minWidth: 100,
-              autoClose: false,
-              closeOnClick: false,
-              className: 'running-popup',
-            })
-          )
-          .openPopup()
-          .setPopupContent('Workout');
+      //Handling clicks on map
+      map.on('click', function (mapE) {
+        mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus();
       });
     },
     function () {
       alert('Could not get your position');
     }
   );
+//THE GLOBAL VARIABLE OF MAP DOES NOT WORK
 
 // EVENT HANDLERS
+// Event Listeners
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  // Clear
 
+  inputDistance.value =
+    inputDuration.value =
+    inputCadence.value =
+    inputElevation.value =
+      '';
+
+  //Display Marker
+  console.log(mapEvent);
+
+  const { lat, lng } = mapEvent.latlng;
+  console.log(lat, lng);
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('Workout')
+    .openPopup();
+});
+
+inputType.addEventListener('change', function () {
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
+
+//
+
+//
+
+//
+
+//
 //===========================================================================//
 // ===== How to Plan a Web Project ===== // VIDEO 231
 // ===== ** ONE OF THE MOST IMPORTANT SKILLS TO LEARN** ===== //
@@ -118,5 +150,9 @@ look at included chart.
 
 
 
+
+*/
+
+/*                        Project Architecture 
 
 */
